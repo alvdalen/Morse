c45c044bcb898cff55b935dcfe9d0a995a6f89e3
Adam Mirzakanov <adamirzakan@gmail.com>
2024-12-14 17:43:35 +0300

    build: initialize the app window

--- Morse/Morse/MainViewController.swift
+++ Morse/Morse/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Morse
 //
 //  Created by Адам Мирзаканов on 14.12.2024.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemOrange
   }
-
-
 }
-
